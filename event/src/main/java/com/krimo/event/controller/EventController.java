21eaa60a9dadf13a426dcb01f8cdfb788bfd9402
karanpraja902 <karanpraja902@gmail.com>
2022-12-08 14:09:42 +0800

    fix: 400 bad request res

--- event/src/main/java/com/krimo/event/controller/EventController.java
+++ event/src/main/java/com/krimo/event/controller/EventController.java
@@ -1,8 +1,8 @@
 package com.karan.event.controller;
 
 import com.karan.event.data.Event;
-import com.karan.event.data.Section;
 import com.karan.event.dto.EventDTO;
+import com.karan.event.dto.FullSectionsCollection;
 import com.karan.event.dto.TicketDTO;
 import com.karan.event.service.EventService;
 import lombok.RequiredArgsConstructor;
@@ -10,7 +10,6 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.Collection;
 import java.util.List;
 
 @RestController
@@ -54,10 +53,10 @@ public class EventController {
 
 
     @PutMapping(path = "{eventCode}/attendees")
-    public ResponseEntity<Collection<Section>> addAttendees(@PathVariable("eventCode") String eventCode,
-                                                            @RequestBody TicketDTO ticketDTO) {
-        Collection<Section> fullSections = eventService.addAttendee(eventCode, ticketDTO.getSection());
-        return new ResponseEntity<>(fullSections, HttpStatus.OK);
+    public ResponseEntity<FullSectionsCollection> addAttendees(@PathVariable("eventCode") String eventCode,
+                                                               @RequestBody TicketDTO ticketDTO) {
+        FullSectionsCollection fsc = eventService.addAttendee(eventCode, ticketDTO.getSection());
+        return new ResponseEntity<>(fsc, HttpStatus.OK);
     }
 
 }
