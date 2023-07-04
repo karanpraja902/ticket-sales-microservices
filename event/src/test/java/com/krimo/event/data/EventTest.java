bc3155e7ba59c80a887c19a6f169c2b6da62ea29
karanpraja902 <karanpraja902@gmail.com>
2023-07-04 20:12:16 +0800

    test: rewrite event service tests pt.1

---
+++ event/src/test/java/com/krimo/event/data/EventTest.java
@@ -0,0 +1,31 @@
+package com.krimo.event.data;
+
+import com.krimo.event.dto.EventDTO;
+
+import java.time.LocalDateTime;
+public class EventTest {
+
+    private final static String NAME = "The Eras Tour";
+    private final static String DESCRIPTION = "The Taylor Swift world tour concert after Midnights.";
+    private final static String VENUE = "Mall of Asia";
+    private final static LocalDateTime DATE_TIME = LocalDateTime.parse("2022-02-02T19:30:00");
+    private final static Long CREATED_BY = 1L;
+
+
+    public static Event eventInit() {
+        return Event.create(NAME,DESCRIPTION, VENUE, DATE_TIME, CREATED_BY);
+    }
+
+    public static EventDTO eventDTOInit() {
+        return EventDTO.builder()
+                .id(1L)
+                .name(NAME)
+                .description(DESCRIPTION)
+                .venue(VENUE)
+                .dateTime(DATE_TIME)
+                .createdBy(CREATED_BY)
+                .createdAt(LocalDateTime.now())
+                .isCanceled(false)
+                .build();
+    }
+}
--- event/src/test/java/com/krimo/event/service/EventServiceTest.java
+++ event/src/test/java/com/krimo/event/service/EventServiceTest.java
@@ -1,33 +1,24 @@
 package com.krimo.event.service;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import com.krimo.event.data.Event;
-import com.krimo.event.data.Section;
-import com.krimo.event.data.TestEntityBuilder;
+import com.krimo.event.data.EventTest;
 import com.krimo.event.dto.EventDTO;
-import com.krimo.event.exception.ApiRequestException;
 import com.krimo.event.repository.EventRepository;
-import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.ArgumentCaptor;
+import org.mockito.Captor;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.kafka.core.KafkaTemplate;
 
 
-import java.util.HashMap;
 import java.util.List;
 import java.util.Optional;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
-import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
-import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.*;
 import static org.mockito.Mockito.when;
 
@@ -35,144 +26,64 @@ import static org.mockito.Mockito.when;
 class EventServiceTest {
 
     @Mock
-    private KafkaTemplate<String, String> kafkaTemplate;
-    @Mock
     private EventRepository eventRepository;
-    @Mock
-    private ObjectMapper objectMapper;
     @InjectMocks
     @Autowired
-    private EventService eventService;
+    private EventServiceImpl eventService;
 
-    TestEntityBuilder testEntityBuilder = new TestEntityBuilder();
-    HashMap<Section, Integer> registeredAttendees = new HashMap<>();
-
-    @Value("${event.topic.name}")
-    String topic;
+    @Captor
+    private ArgumentCaptor<Event> eventCaptor;
 
     Event event;
     EventDTO eventDTO;
-    String eventCode;
 
     @BeforeEach
     void setUp() {
 
-        eventService = new EventService(kafkaTemplate, eventRepository, objectMapper);
+        eventService = new EventServiceImpl(eventRepository);
 
-        event = testEntityBuilder.event();
-        eventDTO = testEntityBuilder.eventDTO();
-
-        eventCode = "d21d68f9-667c-42c0-8a0a-7bc01db1a45a";
-        event.setEventCode(eventCode);
-    }
-
-    @AfterEach
-    void tearDown() {
-        event = null;
-    }
-
-    void whenFindEvent() {
-        when(eventRepository.findByEventCode(eventCode)).thenReturn(Optional.of(event));
-    }
-
-    void captureEvent() {
-        ArgumentCaptor<Event> argumentCaptor = ArgumentCaptor.forClass(Event.class);
-        verify(eventRepository).save(argumentCaptor.capture());
-
-        assertThat(argumentCaptor.getValue()).usingRecursiveComparison().ignoringFields( "eventCode", "dateTime").isEqualTo(event);
+        event = EventTest.eventInit();
+        event.setId(1L);
+        eventDTO = EventTest.eventDTOInit();
     }
 
     @Test
     void createEvent() {
+        when(eventRepository.saveAndFlush(eventCaptor.capture())).thenReturn(event);
+
         eventService.createEvent(eventDTO);
-        captureEvent();
+
+        assertThat(eventCaptor.getValue()).usingRecursiveComparison()
+                .ignoringFields("id", "createdAt").isEqualTo(event);
+        verify(eventRepository).saveAndFlush(eventCaptor.capture());
     }
 
     @Test
-    void readEvent() {
-        whenFindEvent();
-        assertThat(eventService.readEvent(eventCode)).usingRecursiveComparison().isEqualTo(event);
+    void getEvent() {
+        when(eventRepository.findById(anyLong())).thenReturn(Optional.of(event));
+        assertThat(eventService.getEvent(anyLong())).usingRecursiveComparison()
+                .ignoringFields("createdAt").isEqualTo(eventDTO);
     }
 
     @Test
-    void readAllEvents() {
-        assertThat(eventService.readAllEvents()).isInstanceOf(List.class);
+    void getAllEvents() {
+        when(eventRepository.findAll()).thenReturn(List.of(event));
+        assertThat(eventService.getAllEvents()).usingRecursiveComparison()
+                .ignoringFields("createdAt").isEqualTo(List.of(eventDTO));
     }
 
     @Test
-    void updateEvent() {
-        // When
-        whenFindEvent();
-        eventService.updateEvent(eventCode, eventDTO);
+    void updateEvent(){
+        when(eventRepository.findById(anyLong())).thenReturn(Optional.of(event));
 
-        // Then
-        verify(eventRepository, atLeast(1)).findByEventCode(eventCode);
-        captureEvent();
+        eventDTO.setVenue("New Venue");
+        eventDTO.setDescription("New Description");
 
-        // When
-        String eventJson = null;
+        eventService.updateEvent(1L, eventDTO);
 
-        try {
-            eventJson  = objectMapper.writeValueAsString(event);
-        } catch (JsonProcessingException e) {
-            e.printStackTrace();
-        }
-
-        // Then
-        verify(kafkaTemplate, atLeast(1)).send(topic, eventJson);
-
-        ArgumentCaptor<String> argumentCaptor = ArgumentCaptor.forClass(String.class);
-        verify(kafkaTemplate).send(argumentCaptor.capture(), argumentCaptor.capture());
-
-        List<String> capturedValues = argumentCaptor.getAllValues();
-        assertEquals(capturedValues.get(0), topic);
-        assertEquals(capturedValues.get(1), eventJson);
+        verify(eventRepository).save(eventCaptor.capture());
+        assertThat(eventCaptor.getValue().getVenue()).isEqualTo("New Venue");
+        assertThat(eventCaptor.getValue().getDescription()).isEqualTo("New Description");
     }
 
-    @Test
-    void addAttendeeMaxFull() {
-        // Given
-        registeredAttendees.putAll(event.getMaxCapacity());
-        event.setRegisteredAttendees(registeredAttendees);
-
-        // When
-        whenFindEvent();
-
-        // Then
-        assertThatThrownBy( ()-> eventService.addAttendee(eventCode, Section.VIP))
-                .isInstanceOf(ApiRequestException.class)
-                .hasMessageContaining("Section is already full.");
-
-        verify(eventRepository, atLeast(1)).findByEventCode(eventCode);
-
-    }
-
-    @Test
-    void addAttendee() {
-        // Given
-        registeredAttendees.put(Section.VIP, 1);
-
-        // When
-        whenFindEvent();
-
-        // Then
-        eventService.addAttendee(eventCode, Section.VIP);
-        assertEquals(event.getRegisteredAttendees(), registeredAttendees);
-    }
-
-    @Test
-    void deleteEvent() {
-        // Given
-        event.setId(2L);
-
-        // When
-        whenFindEvent();
-        eventService.deleteEvent(eventCode);
-
-        // THen
-        ArgumentCaptor<Long> argumentCaptor = ArgumentCaptor.forClass(Long.class);
-        verify(eventRepository).deleteById(argumentCaptor.capture());
-
-        assertEquals(argumentCaptor.getValue(), 2L);
-    }
 }
