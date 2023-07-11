6d6845a3aa4bc72564b5278bc616044e46419f82
karanpraja902 <karanpraja902@gmail.com>
2023-07-11 22:58:20 +0800

    feat: debezium connector for the outbox table, open permissions

--- db-init-scripts/schema.sql
+++ db-init-scripts/schema.sql
@@ -12,3 +12,16 @@ GRANT ALL PRIVILEGES ON DATABASE event_ticket_db TO userticket;
 REVOKE ALL PRIVILEGES ON SCHEMA public FROM userevent, userticket;
 GRANT USAGE ON SCHEMA public TO userevent, userticket;
 GRANT ALL PRIVILEGES ON SCHEMA public TO userevent, userticket;
+
+
+    -- outbox table for all microservices
+CREATE TABLE IF NOT EXISTS public.outbox (
+ id UUID NOT NULL,
+ topic varchar(255) NOT NULL,
+payload varchar(4096) NOT NULL,
+timestamp TIMESTAMP,
+CONSTRAINT pkey PRIMARY KEY (id)
+);
+
+REVOKE ALL PRIVILEGES ON public.outbox FROM userevent, userticket;
+GRANT ALL PRIVILEGES  ON public.outbox TO userevent, userticket;
