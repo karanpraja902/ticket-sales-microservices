851b4401fc39a8355c2068abf17e8ee55d1f0a1a
karanpraja902 <karanpraja902@gmail.com>
2023-07-26 00:18:57 +0800

    fix: update logic in user profile service + add delete func

--- userprofile/src/main/java/com/krimo/userprofile/repository/UserProfileRepository.java
+++ userprofile/src/main/java/com/krimo/userprofile/repository/UserProfileRepository.java
@@ -2,6 +2,7 @@ package com.karan.userprofile.repository;
 
 import com.karan.userprofile.domain.UserProfile;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
