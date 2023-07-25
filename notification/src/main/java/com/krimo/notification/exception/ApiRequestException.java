8480c226bac5d445327ec2f2bbb1f100f4c51509
karanpraja902 <karanpraja902@gmail.com>
2023-07-25 14:44:20 +0800

    test: add notification service tests

--- notification/src/main/java/com/krimo/notification/exception/ApiRequestException.java
+++
@@ -1,7 +0,0 @@
-package com.karan.notification.exception;
-
-public class ApiRequestException extends RuntimeException {
-    public ApiRequestException(String message) {
-        super(message);
-    }
-}
--- notification/src/main/java/com/krimo/notification/service/ClientService.java
+++ notification/src/main/java/com/krimo/notification/service/ClientService.java
@@ -1,7 +1,6 @@
 package com.karan.notification.service;
 
 import com.karan.notification.client.UserProfileClient;
-import com.karan.notification.exception.ApiRequestException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
@@ -19,10 +18,6 @@ class UserProfileClientService implements ClientService {
     @Override
     public String getEmail(Long id){
         ResponseEntity<String> response = userProfileClient.getUserEmail(id);
-
-        if (!response.getStatusCode().is2xxSuccessful()) {
-            throw new ApiRequestException("Failed to get user email");
-        }
 
         return response.getBody();
     }
